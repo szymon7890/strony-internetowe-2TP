6f8400e312e3f86a70d1250283019241b9c845f5
Szymon <80423080+szymon7890@users.noreply.github.com>
2023-11-18 09:26:15 +0100

    Add files via upload

---
+++ 2 TP strony internetowe semestr 1/PHP 01.11.2021/cars.php
@@ -0,0 +1,33 @@
+<?php
+// Początek bloku kodu PHP
+
+	// Wyświetlenie zawartości tablicy $_POST
+	print_r($_POST);
+
+	// Tworzenie połączenia z bazą danych MySQL
+	$conn = new mysqli("localhost", "root", "", "serwis");
+
+	// Sprawdzenie, czy połączenie z bazą danych się udało
+	if ($conn->connect_errno) {
+		echo "Failed to connect to MySQL: " . $conn->connect_error;
+		exit();
+	}
+
+	// Przykład kodu, który jest zakomentowany (nieaktywny) - próba wstawienia danych do bazy danych
+	$sql = "INSERT INTO `samochody`(`id_samochodu`, `marka_samochodu`, `model_samochodu`, `rocznik`, `uzywany`) VALUES ('1', '2', '3', '2021-10-06 12:00:00', '5')";
+
+	// Wykonanie zapytania SQL (wstawienie danych do bazy danych)
+	$result = $conn->query($sql);
+
+	// Sprawdzenie, czy zapytanie się powiodło
+	if ($result === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	// Zamknięcie połączenia z bazą danych
+	$conn->close();
+
+// Koniec bloku kodu PHP
+?>
